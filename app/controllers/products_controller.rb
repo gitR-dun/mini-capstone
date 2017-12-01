16d6a3cd94673941587e223ea5d131f5d90f4918
Brian <riceb53@gmail.com>
2017-11-30 19:48:47 -0600

    make urls for product and products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,18 @@ class ProductsController < ApplicationController
     # show em
     render json: products.as_json
   end
+
+  def one_product
+    # get ONE product from the db
+    product = Product.first
+    # show that product to the user
+    render json: product.as_json
+  end
+
+  def two_product
+    # get ONE product from the db
+    product = Product.second
+    # show that product to the user
+    render json: product.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get '/all_products' => 'products#the_products'
+  get '/the_product' => 'products#one_product'
+  get '/second_product' => 'products#two_product'
 end
