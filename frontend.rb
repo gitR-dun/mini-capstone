b2c2df7ef8f451f9ea4f7e1ddaf5f73df98c5de1
Brian <riceb53@gmail.com>
2017-11-30 19:52:31 -0600

    add tty-prompt gem

--- frontend.rb
+++ frontend.rb
@@ -1,14 +1,21 @@
 require 'unirest'
-
+require 'tty-prompt'
 # make an HTTP request to bring back the data on my products
 
 response = Unirest.get("localhost:3000/all_products")
 
 products = response.body
 
-products.each do |product|
-  p 'here is the product\'s name'
-  p product['name']
-end
+prompt = TTY::Prompt.new
+
+user_input = prompt.select("Which product would you like?", ["blender", "hair dryer", "chess set"])
+
+
+p user_input
+
+# products.each do |product|
+#   p 'here is the product\'s name'
+#   p product['name']
+# end
 
 
