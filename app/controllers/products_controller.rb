822360ef6aaad530c6847aeadf13bcdb0aea9c52
Brian <riceb53@gmail.com>
2017-12-07 16:17:17 -0600

    add routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,8 @@
 class ProductsController < ApplicationController
   def the_products
+    p "here is the info from my params hash"
+    p params["message"]
+    p "here is the info from my params hash"
     # show the user all the products
     # get all the products
     products = Product.all
@@ -18,6 +21,20 @@ class ProductsController < ApplicationController
     # get ONE product from the db
     product = Product.second
     # show that product to the user
+    render json: product.as_json
+  end
+
+  def show
+    the_product_name = params['name']
+    product = Product.find_by(name: the_product_name)
+    render json: product.as_json
+  end
+
+  def segment
+    # get some user input the key in the params hash
+    # use that info to grab something from the db
+    the_id = params['id']
+    product = Product.find_by(id: the_id)
     render json: product.as_json
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get '/all_products' => 'products#the_products'
   get '/the_product' => 'products#one_product'
   get '/second_product' => 'products#two_product'
+  get '/show-the-product' => 'products#show'
+  get '/one-of-the-products/:id' => 'products#segment'
 end
