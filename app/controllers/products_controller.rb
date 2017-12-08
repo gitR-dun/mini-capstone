6a4ec89aeae15a36bcb6d056a80df4bf65b8d76c
Brian <riceb53@gmail.com>
2017-12-07 19:26:47 -0600

    make routes RESTful

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,5 @@
 class ProductsController < ApplicationController
-  def the_products
-    p "here is the info from my params hash"
-    p params["message"]
-    p "here is the info from my params hash"
+  def index
     # show the user all the products
     # get all the products
     products = Product.all
@@ -10,31 +7,10 @@ class ProductsController < ApplicationController
     render json: products.as_json
   end
 
-  def one_product
-    # get ONE product from the db
-    product = Product.first
-    # show that product to the user
-    render json: product.as_json
-  end
-
-  def two_product
-    # get ONE product from the db
-    product = Product.second
-    # show that product to the user
-    render json: product.as_json
-  end
-
   def show
-    the_product_name = params['name']
-    product = Product.find_by(name: the_product_name)
-    render json: product.as_json
-  end
-
-  def segment
-    # get some user input the key in the params hash
-    # use that info to grab something from the db
-    the_id = params['id']
-    product = Product.find_by(id: the_id)
+    # get ONE product from the db
+    product = Product.find_by(id: params[:id])
+    # show that product to the user
     render json: product.as_json
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get '/all_products' => 'products#the_products'
-  get '/the_product' => 'products#one_product'
-  get '/second_product' => 'products#two_product'
-  get '/show-the-product' => 'products#show'
-  get '/one-of-the-products/:id' => 'products#segment'
+  get '/products' => 'products#index'
+  get '/products/:id' => 'products#show'
 end
