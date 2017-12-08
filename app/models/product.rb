5721f7a209447010419bd055246e35c153e1c946
Brian <riceb53@gmail.com>
2017-12-07 19:48:49 -0600

    add model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,27 @@
 class Product < ApplicationRecord
+  def as_json
+    {
+      id: id,
+      name: name,
+      price: price,
+      description: description,
+      image: image,
+      is_discounted?: is_discounted?,
+      tax: tax,
+      total: total
+    }
+  end
 
+  def is_discounted?
+    # if the price of THIS PARTICULAR PRODUCT is less than $5 return true, otherwise return false
+    price < 5
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    price + tax
+  end
 end
